bd40bdad3e33a0f29083b0e69df1cb5fbf4270e9
joseph townshend <joetownshend@mac.com>
2019-06-03 16:32:25 +0100

    top up amount with 1 argument and tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,14 @@ class Oystercard
 
   attr_reader :balance
 
+  BALANCE_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
-  def top_up
-    5
+  def top_up(amount)
+    @balance += amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,11 +7,19 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  it "can top up the balance" do
-    oystercard = Oystercard.new
-    expect(subject.top_up).to eq(5)
-  end
+  #it "can top up the balance" do
+  #  oystercard = Oystercard.new
+  #  expect(subject.top_up).to eq(nil)
+  #end
 
+  describe '#top_up' do
+
+     it { is_expected.to respond_to(:top_up).with(1).argument }
+
+     it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+   end
 
 
 end
