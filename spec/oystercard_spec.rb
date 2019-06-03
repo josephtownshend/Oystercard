84cc2fb26b55a33de1050f889cb69f6d617f8e96
joseph townshend <joetownshend@mac.com>
2019-06-03 15:40:38 +0100

    addition of @balance and top up def

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,17 @@
+require 'oystercard'
 
 describe Oystercard do
-  
+
+  it "has a balance" do
+    oystercard = Oystercard.new
+    expect(subject.balance).to eq(0)
+  end
+
+  it "can top up the balance" do
+    oystercard = Oystercard.new
+    expect(subject.top_up).to eq(5)
+  end
+
+
+
 end
