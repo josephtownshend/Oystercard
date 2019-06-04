9f9ccbd5f1044e563c6ebc6cb203b18adfccce27
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-04 15:45:03 +0100

    touch in/out, in_journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,10 +6,27 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @active = false
   end
 
   def top_up(amount)
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @active = true
+  end
+
+  def touch_out
+    @active = false
+  end
+
+  def in_journey?
+    @active
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,19 +7,40 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  #it "can top up the balance" do
-  #  oystercard = Oystercard.new
-  #  expect(subject.top_up).to eq(nil)
-  #end
-
   describe '#top_up' do
 
-     it { is_expected.to respond_to(:top_up).with(1).argument }
+     # it { is_expected.to respond_to(:top_up).with(1).argument }
 
      it 'can top up the balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
-   end
 
+  end
 
+  describe 'deduct' do
+    it 'can deduct money from the card' do
+    expect{ subject.deduct 5 }.to change{ subject.balance }.by -5
+    end
+  end
+
+  describe 'in_journey' do
+    it 'oystecard active if touch_in' do
+      expect(subject.touch_in).to eq true
+    end
+
+    it 'oystercard in-active if touch_out' do
+      expect(subject.touch_out).to eq false
+    end
+
+    it 'oystercard active, in-journey' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'oystercard in-active, not-in-journey' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 end
